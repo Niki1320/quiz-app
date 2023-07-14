263bd801fef514b05223ff59bd28a141c8f06732
Nikitha V <nikithav678@gmail.com>
2023-07-14 14:52:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import Auth from './components/Auth/Auth';
+import Dashboard from './components/Dashboard/Dashboard';
+import CreateQuiz from './components/CreateQuiz/CreateQuiz';
+import MyQuizzes from './components/MyQuizzes/MyQuizzes';
+import CommunityQuizzes from './components/CommunityQuizzes/CommunityQuizzes';
+import ViewQuiz from './components/ViewQuiz/ViewQuiz';
+import TakeQuiz from './components/TakeQuiz/TakeQuiz';
+import ViewResults from './components/ViewResults/ViewResults';
+import Profile from './components/Profile/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import store from './store';
+
+class App extends React.Component {
+
+  componentDidMount() {
+    if (localStorage.getItem('_ID')) {
+      axios.get(`/api/users/${localStorage.getItem('_ID')}`).then(res => {
+        store.dispatch({
+          user: res.data.user,
+          type: 'set_user'
+        })
+      }).catch(er => {
+        console.log(er);
+      })
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Auth}/>
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/create-quiz" component={CreateQuiz} />
+            <Route path="/my-quizzes" component={MyQuizzes} />
+            <Route path="/community-quizzes" component={CommunityQuizzes} />
+            <Route path="/view-quiz" component={ViewQuiz} />
+            <Route path="/take-quiz" component={TakeQuiz} />
+            <Route path="/view-results" component={ViewResults} />
+            <Route path="/account" component={Profile} />
+            <Route path="*">
+              <Redirect to="/" />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    )
+  }
 }
 
 export default App;
